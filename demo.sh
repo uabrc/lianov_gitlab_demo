25e27b03995b81443886161fce7dc74c72004054
Lara Ianov <lianov@uab.edu>
2018-11-28 14:28:14 -0600

    Changes made in Atom and committed etc. directly

--- demo.sh
+++ demo.sh
@@ -5,3 +5,8 @@ echo "demo test for gitlab"
 echo "printing working directory, `pwd`"
 
 echo "line added locally after cloning, and then committed and pushed to GitLab"
+
+# A good FYI about the Atom editor:
+# changes below were made in Atom, staged, committed and pushed directly from Atom
+echo "printing current date and time"
+date
