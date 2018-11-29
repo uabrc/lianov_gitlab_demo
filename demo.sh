caeed48f5bac94ed788957e7159f333ad6da160f
Lara Ianov <lianov@uab.edu>
2018-11-29 14:08:21 -0600

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -10,3 +10,5 @@ echo "line added locally after cloning, and then committed and pushed to GitLab"
 # changes below were made in Atom, staged, committed and pushed directly from Atom
 echo "printing current date and time"
 date
+
+#----- This represents finalized code ready to be merged --------
