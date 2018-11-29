a6497b9d7d9f5461b4a23f00b63482e983542e13
Lara Ianov <lianov@uab.edu>
2018-11-29 14:55:21 -0600

    hostname added

--- demo.sh
+++ demo.sh
@@ -8,9 +8,9 @@ echo "line added locally after cloning, and then committed and pushed to GitLab"
 
 # A good FYI about the Atom editor:
 # changes below were made in Atom, staged, committed and pushed directly from Atom
-echo "printing current date and time"
-date
+echo "printing current date and time, `date`"
 
 #----- This represents finalized code ready to be merged --------
 
 #---- This represents work in progress, thus it remains in the branch for now -----
+echo "printing hostname, `hostname`"
